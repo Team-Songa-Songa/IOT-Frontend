56922ffac0d64861933a1a2b0db9d9e2bcd207d5
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-09 15:47:11 +0300

    project template

---
+++ src/pages/Authentication/Register/register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Register from "../../../components/Authentication/Register/register";
+function RegisterPage() {
+  return (
+    <>
+      <Register />
+    </>
+  );
+}
+
+export default RegisterPage;
