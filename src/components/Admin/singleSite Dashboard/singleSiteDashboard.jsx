5f001c060a08d8fed6bfe9296527a8002fb7e9da
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-16 12:10:32 +0300

    underway with the dashboard

--- src/components/Admin/singleSite Dashboard/singleSiteDashboard.jsx
+++ src/components/Admin/singleSite Dashboard/singleSiteDashboard.jsx
@@ -6,67 +6,49 @@ function SingleSiteDashboard() {
     <>
       <section className="single-site">
         <div className="single-site-inner">
+          <h1 className="single-site-h1">
+            Wanjie's Greenhouse Cabbage Control
+          </h1>
 
-          
-
-            <h1 className="single-site-h1">
-              Wanjie's Greenhouse Cabbage Control
-            </h1>
-
-            <div className="minor-navs">
-              <Link to='' className="minor-navs-inner-p">
+          <div className="minor-navs">
+            <Link to="" className="minor-navs-inner-p">
               <div className="minor-navs-inner">
-                <p >Dashboard</p>
+                <p>Dashboard</p>
               </div>
-              </Link>
-              <Link to='' className="minor-navs-inner-p">
-
+            </Link>
+            <Link to="" className="minor-navs-inner-p">
               <div className="minor-navs-inner">
-                <p >Notifications</p>
+                <p>Notifications</p>
               </div>
-              </Link>
-              <Link to=''className="minor-navs-inner-p" >
-
+            </Link>
+            <Link to="" className="minor-navs-inner-p">
               <div className="minor-navs-inner">
-                <p >Management</p>
+                <p>Management</p>
               </div>
-              </Link>
+            </Link>
+          </div>
 
+          <div className="main-dashboard">
+            <div className="upper-stats">
+              <p className="upper-stats-hours">1103 Hours</p>
+              <p className="upper-stats-text">Operating Time Hours</p>
             </div>
 
-            <div className="main-dashboard">
-
-              <div className="upper-stats">
-
-                <p className="upper-stats-hours">1103 Hours</p>
-                <p className="upper-stats-text">Operating Time Hours</p>
-
-              </div>
-
-              <div className="upper-stats">
-
-                <p>1103 Hours</p>
-                <p>Operating Time Hours</p>
-
-              </div>
-
-              <div className="upper-stats">
-
-                <p>1103 Hours</p>
-                <p>Operating Time Hours</p>
-
-              </div>
-
-
-
+            <div className="upper-stats">
+              <p className="upper-stats-hours">1103 kWh</p>
+              <p className="upper-stats-text">Energy Consumption</p>
             </div>
 
-          
+            <div className="upper-stats">
+              <p className="upper-stats-hours">1103 L</p>
+              <p className="upper-stats-text">Water Produced (* 1000 L)</p>
+            </div>
 
-
-
-
-
+            <div className="upper-stats">
+              <p className="upper-stats-hours">1103 PF</p>
+              <p className="upper-stats-text">Power Factor</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
