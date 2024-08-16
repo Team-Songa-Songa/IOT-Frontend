e0f9d6638cf1a5d6e6a9d401360eb33808744b0b
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-16 12:37:18 +0300

    underway with the dashboard

--- src/components/Admin/singleSite Dashboard/singleSiteDashboard.jsx
+++ src/components/Admin/singleSite Dashboard/singleSiteDashboard.jsx
@@ -1,7 +1,74 @@
 import React from "react";
 import "./singleSite.css";
 import { Link } from "react-router-dom";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
 function SingleSiteDashboard() {
+  const chartOptions1 = {
+    chart: {
+      type: "pie",
+      innerSize: "50%",
+      backgroundColor: "transparent",
+      // width: 350,
+      // height: 400,
+    },
+    title: {
+      text: "",
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: true,
+          format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+          style: {
+            color: "black",
+          },
+        },
+      },
+    },
+    series: [
+      {
+        name: "Share",
+        data: [
+          { name: "Automatic Restarts", y: 58.8, color: "#6e44ff" },
+          { name: "Starts Counter", y: 41.2, color: "#00aaff" },
+        ],
+      },
+    ],
+  };
+
+  const chartOptions2 = {
+    chart: {
+      type: "pie",
+      innerSize: "50%",
+      backgroundColor: "transparent",
+    },
+    title: {
+      text: "",
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: true,
+          format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+          style: {
+            color: "black",
+          },
+        },
+      },
+    },
+    series: [
+      {
+        name: "Share",
+        data: [
+          { name: "Power On Time", y: 44.2, color: "#6e44ff" },
+          { name: "Operating Time", y: 55.8, color: "#00aaff" },
+        ],
+      },
+    ],
+  };
+
   return (
     <>
       <section className="single-site">
@@ -50,20 +117,23 @@ function SingleSiteDashboard() {
             </div>
 
             <div className="upper-stats charts">
-
-              <p className="charts-p">Number of Automatic Restarts After trips vs Number of starts counter</p>
-
-              
-
-
-             
-
-
+              <h2>
+                Number of Automatic Restarts After Trips vs Number of Starts
+                Counter
+              </h2>
+              <HighchartsReact
+                highcharts={Highcharts}
+                options={chartOptions1}
+              />
             </div>
 
-           
-
-
+            <div className="upper-stats charts">
+              <h2>Power On Time Counter vs Operating Time Counter</h2>
+              <HighchartsReact
+                highcharts={Highcharts}
+                options={chartOptions2}
+              />
+            </div>
           </div>
         </div>
       </section>
