522afd34628510f2fbc758db439d2e1f3e4a9607
MuchiriAndrew <kariukia225@gmail.com>
2024-08-17 22:21:10 +0300

    refactor

--- src/pages/Admin/singleSiteDashboard.jsx
+++ src/pages/Admin/singleSiteDashboard.jsx
@@ -6,42 +6,42 @@ import { Grid } from '@mui/material';
 
 function SingleSiteDashboardPage() {
   // Example data for the graphs
-//   const lineChartData = {
-//     labels: ["January", "February", "March", "April", "May", "June"],
-//     datasets: [
-//       {
-//         label: "Dataset 1",
-//         data: [65, 59, 80, 81, 56, 55],
-//         borderColor: "yellow",
-//         backgroundColor: "rgba(255, 99, 132, 0.5)",
-//         width: 20,
+  const lineChartData = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [65, 59, 80, 81, 56, 55],
+        borderColor: "yellow",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        width: 20,
 
 
-//       },
-//       {
-//         label: "Dataset 2",
-//         data: [28, 48, 40, 19, 86, 27],
-//         borderColor: "rgb(54, 162, 235)",
-//         backgroundColor: "rgba(54, 162, 235, 0.5)",
-//       },
-//     ],
-//   };
+      },
+      {
+        label: "Dataset 2",
+        data: [28, 48, 40, 19, 86, 27],
+        borderColor: "rgb(54, 162, 235)",
+        backgroundColor: "rgba(54, 162, 235, 0.5)",
+      },
+    ],
+  };
 
-//   const barChartData = {
-//     labels: ["Category 1", "Category 2", "Category 3", "Category 4"],
-//     datasets: [
-//       {
-//         label: "Dataset 1",
-//         data: [65, 59, 80, 81],
-//         backgroundColor: [
-//           "rgba(255, 99, 132, 0.5)",
-//           "rgba(54, 162, 235, 0.5)",
-//           "rgba(255, 206, 86, 0.5)",
-//           "rgba(75, 192, 192, 0.5)",
-//         ],
-//       },
-//     ],
-//   };
+  const barChartData = {
+    labels: ["Category 1", "Category 2", "Category 3", "Category 4"],
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [65, 59, 80, 81],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+        ],
+      },
+    ],
+  };
 
   return (
     <div>
