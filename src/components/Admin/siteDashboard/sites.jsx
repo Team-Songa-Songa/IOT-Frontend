b1bec24f2d92591f9dbdc46b2f6e43951a6acf64
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-08-25 14:10:05 +0300

    sites

--- src/components/Admin/siteDashboard/sites.jsx
+++ src/components/Admin/siteDashboard/sites.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import './sites.css'
+import React from "react";
+import "./sites.css";
 function Sites() {
   return (
     <>
-    <div>sites</div>
+      <section className="sites-section">
+        <p className="sites-section-p">Your Sites</p>
+
+        <div className="sites-individual">
+          <div className="site-individual-sites">
+            <p>Nawasco Soil Pump station</p>
+          </div>
+
+          <div className="sites-buttons">
+            <div className="site-buttons-left">
+              <button>View Site</button>
+            </div>
+            <div className="site-buttons-right">Edit</div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Sites
+export default Sites;
